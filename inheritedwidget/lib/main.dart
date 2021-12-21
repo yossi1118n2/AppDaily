7926379b27bc35b4ef8642fb0ebe4e47e4e0c0bc
yossi1118n2 <yossimaster1401@gmail.com>
2021-12-21 18:22:18 +0900

    InheritedWidgetに関する学習コード
    providerを使った場合

--- inheritedwidget/lib/main.dart
+++ inheritedwidget/lib/main.dart
@@ -1,49 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(const App());
+void main() => runApp(App());
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:  _HomePage(),
-    );
-  }
-}
-
-class _HomePage extends StatefulWidget {
-  const _HomePage({Key? key}) : super(key: key);
-
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<_HomePage> {
-  var _count = 1;
-
-  @override
-  Widget build(BuildContext context) {
-    return _Inherited(
-      message: _createMessage(),
-      child: Scaffold(
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.refresh),
-          onPressed: () => setState(() => _count++),
-        ),
-        body: Center(
-          child: _Message(),
-        ),
+      home: ChangeNotifierProvider(
+        builder: (context) => _FizzBuzz(),
+        create: (BuildContext context) {  },
+        child: _HomePage(),
       ),
     );
   }
+}
 
-  String _createMessage() {
-    final result = _count % 15 == 0
-        ? 'FizzBuzz'
-        : (_count % 3 == 0 ? 'Fizz' : (_count % 5 == 0 ? 'Buzz' : '-'));
-    print('_count: $_count, result: $result');
-    return result;
+class _HomePage extends StatelessWidget {
+  const _HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.refresh),
+        onPressed: () => Provider.of<_FizzBuzz>(
+          context,
+          listen: false,
+        ).increment(),
+      ),
+      body: Center(
+        child: const _Message(),
+      ),
+    );
   }
 }
 
@@ -53,36 +43,22 @@ class _Message extends StatelessWidget {
   Widget build(BuildContext context) {
     print('_Message: rebuild');
     return Text(
-      'Message: ${_Inherited.of(
-        context,
-        // 変更監視する
-        listen: true,
-      )!.message}',
+      'Message: ${Provider.of<_FizzBuzz>(context).message}',
       style: TextStyle(fontSize: 64),
     );
   }
 }
 
-class _Inherited extends InheritedWidget {
-  const _Inherited({
-    Key? key,
-    required this.message,
-    required Widget child,
-  }) : super(key: key, child: child);
+class _FizzBuzz extends ValueNotifier<int> {
+  _FizzBuzz() : super(1);
 
-  final String message;
+  void increment() => value++;
 
-  static _Inherited? of(
-      BuildContext context, {
-        required bool listen,
-      }) {
-    return listen
-        ? context.dependOnInheritedWidgetOfExactType<_Inherited>()
-        : context
-        .getElementForInheritedWidgetOfExactType<_Inherited>()!
-        .widget as _Inherited;
+  String get message {
+    final result = value % 15 == 0
+        ? 'FizzBuzz'
+        : (value % 3 == 0 ? 'Fizz' : (value % 5 == 0 ? 'Buzz' : '-'));
+    print('value: $value, result: $result');
+    return result;
   }
-
-  @override
-  bool updateShouldNotify(_Inherited old) => message != old.message;
 }
